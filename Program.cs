a31a57b9885414b9a8bd0d3d3bf350b5cc1265b5
Giovanni Bassi <giggio@giggio.net>
2023-10-13 23:27:54 -0300

    Support --os and refactor

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
 using Cross;
 using Docker.DotNet;
 using Docker.DotNet.Models;
-using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Formats.Tar;
 using System.Text.Json;
 using System.Text.RegularExpressions;
 using static System.Console;
+using static Program;
 
 var cancellationTokenSource = new CancellationTokenSource();
 var cancellationToken = cancellationTokenSource.Token;
@@ -16,29 +16,32 @@ CancelKeyPress += (_, eventArgs) =>
     cancellationTokenSource.Cancel();
 };
 
-if (!TryGetRid(args, out var rid))
-    return Die("Invalid RID.");
-if (rid == null)
+var (rid, os, isInvalidOptions, isLinux, isMusl) = Options.Create(args);
+
+if (isInvalidOptions)
+    return Die("Invalid options.");
+if (rid is null && os is null)
 {
     WriteLine("Running dotnet directly.");
     return await RunDotnetAsync(args, cancellationToken);
 }
-
+var currentRID = System.Runtime.InteropServices.RuntimeInformation.RuntimeIdentifier;
+if (Environment.Version.Major >= 8 && (currentRID == rid || IsItMusl(rid, os) == IsItMusl(currentRID, null)))
+{
+    WriteLine("No need to run in container, RID/OS matches, running dotnet directly.");
+    return await RunDotnetAsync(args, cancellationToken);
+}
+if (!isLinux)
+    return Die("Only Build for Linux is supported.");
 const string sdkVersion = "8.0";
-ReadOnlyCollection<Image> imageList = new(
-[
-    new("linux-musl-x64", $"mcr.microsoft.com/dotnet/sdk:{sdkVersion}-alpine", "apk add clang build-base zlib-dev docker-cli"),
-    new("linux-x64", $"mcr.microsoft.com/dotnet/sdk:{sdkVersion}-jammy",
+var imageMusl = new Image("musl", $"mcr.microsoft.com/dotnet/sdk:{sdkVersion}-alpine", "apk add clang build-base zlib-dev docker-cli");
+var imageGlibc = new Image("glibc", $"mcr.microsoft.com/dotnet/sdk:{sdkVersion}-jammy",
         "apt-get update && apt-get install -y clang zlib1g-dev curl gnupg",
         "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg && chmod a+r /etc/apt/keyrings/docker.gpg",
         """echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" > /etc/apt/sources.list.d/docker.list""",
-        "apt-get update && apt-get install -y docker-ce-cli")
-]);
-var images = imageList.ToDictionary(i => i.RID, i => i);
-if (!images.TryGetValue(rid, out var image))
-    return Die($"Runtime identifier (RID) not supported: {rid}.");
-var imageTag = $"dotnet-build:{sdkVersion}-{image.RID}";
-var targetIsWindows = rid.StartsWith("win");
+        "apt-get update && apt-get install -y docker-ce-cli");
+var image = isMusl ? imageMusl : imageGlibc;
+var imageTag = $"dotnet-build:{sdkVersion}-{image.LibC}";
 using var client = new DockerClientConfiguration().CreateClient();
 try
 {
@@ -69,40 +72,6 @@ catch (Exception ex)
     return Die($"Got unexpected error: {ex.Message}.");
 }
 return 0;
-
-static bool TryGetRid(string[] args, out string? rid)
-{
-    var ridIndex = Array.IndexOf(args, "-r");
-    if (ridIndex == -1)
-        Array.IndexOf(args, "--runtime");
-    if (ridIndex > -1)
-    {
-        if (args.Length > ridIndex + 1)
-        {
-            var ridCandidate = args[ridIndex + 1];
-            if (ridCandidate != null && !ridCandidate.StartsWith('-'))
-            {
-                rid = ridCandidate;
-            }
-            else
-            {
-                rid = null;
-                return false;
-            }
-        }
-        else
-        {
-            rid = null;
-            return false;
-        }
-    }
-    else
-    {
-        var ridCandidate = args.FirstOrDefault(arg => arg.StartsWith("-r=") || arg.StartsWith("--runtime"));
-        rid = ridCandidate ?? null;
-    }
-    return true;
-}
 
 async Task<int> RunDotnetAsync(string[] args, CancellationToken cancellationToken)
 {
@@ -162,7 +131,7 @@ RUN mkdir /home/user
 {(OperatingSystem.IsLinux() ? $"RUN chown {Linux.getuid()}:{Linux.getgid()} /home/user" : "# chown only for Linux")}
 """";
     const string dockerfileName = "Dockerfile";
-    var dockerfileDirectory = Path.Join(Path.GetTempPath(), $"dotnet-build_{sdkVersion}-{image.RID}");
+    var dockerfileDirectory = Path.Join(Path.GetTempPath(), $"dotnet-build_{sdkVersion}-{image.LibC}");
     var dockerfilePath = Path.Join(dockerfileDirectory, dockerfileName);
     try
     {
@@ -236,6 +205,7 @@ async Task<string> CreateContainerNameAsync()
             return match.Success ? int.Parse(match.Groups[1].Value) : 0;
         })
         .Where(id => id > 0)
+        .Append(0)
         .Max();
     var containerName = $"dotnet-build-{++buildContainersRunning:0000}";
     return containerName;
@@ -258,7 +228,8 @@ async Task<string?> RunContainerAsync(string containerName)
         HostConfig = new()
         {
             Binds = [
-                targetIsWindows ? "//./pipe/docker_engine://./pipe/docker_engine" : "/var/run/docker.sock:/var/run/docker.sock",
+                // todo: should we ever do this? It fails on Windows, for me: OperatingSystem.IsWindows() ? "//./pipe/docker_engine://./pipe/docker_engine" : "/var/run/docker.sock:/var/run/docker.sock",
+                "/var/run/docker.sock:/var/run/docker.sock",
                 $"{Environment.CurrentDirectory}:/app"
             ]
         }
@@ -293,10 +264,64 @@ int Die(params string[] messages)
     return 1;
 }
 
-internal record struct Image(string RID, string Tag, params string[] BuildCommands);
 
 public partial class Program
 {
     [GeneratedRegex("^/dotnet-build-(\\d{4})$")]
     private static partial Regex DotnetBuildContainer();
+
+    public static bool IsItMusl(string? rid, string? os) => (rid != null && rid.StartsWith("linux-musl-")) || os == "linux-musl";
+}
+
+internal record struct Image(string LibC, string Tag, params string[] BuildCommands);
+
+internal record struct Options(string? RID, string? OS, bool InvalidOptions, bool IsLinux, bool IsMusl)
+{
+    public static Options Create(string[] args)
+    {
+        var validOptions = TryGetRid(args, out var rid)
+            & TryGetOS(args, out var os);
+        var isLinux = (rid != null && (rid == "linux" || rid.StartsWith("linux-"))) || (os is not null && (os == "linux" || os.StartsWith("linux-")));
+        // we have 2 big Linux families: glibc and musl (in .NET they are 2 OSs: "linux" and "linux-musl")
+        var isMusl = isLinux && IsItMusl(rid, os);
+        return new(rid, os, !validOptions, isLinux, isMusl);
+    }
+
+    private static bool TryGetRid(string[] args, out string? rid) => TryGetOption(args, out rid, "-r", "--runtime");
+
+    private static bool TryGetOS(string[] args, out string? os) => TryGetOption(args, out os, null, "--os");
+
+    private static bool TryGetOption(string[] args, out string? value, string? shortOption, string longOption)
+    {
+        var index = Array.IndexOf(args, shortOption);
+        if (index == -1)
+            index = Array.IndexOf(args, longOption);
+        if (index > -1)
+        {
+            if (args.Length > index + 1)
+            {
+                var candidate = args[index + 1];
+                if (candidate != null && !candidate.StartsWith('-'))
+                {
+                    value = candidate;
+                }
+                else
+                {
+                    value = null;
+                    return false;
+                }
+            }
+            else
+            {
+                value = null;
+                return false;
+            }
+        }
+        else
+        {
+            var candidate = args.FirstOrDefault(arg => arg.StartsWith($"{shortOption}=") || arg.StartsWith($"{longOption}="));
+            value = candidate?[(candidate.IndexOf('=') + 1)..];
+        }
+        return true;
+    }
 }
